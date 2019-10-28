4143808eab281c67c55fcf0b838060cbcf9500fe
Dan Pope <danielthepope@users.noreply.github.com>
2019-10-28 16:50:44 +0000

    MSPA-427: Add sequenceNumber and sequenceIdentifier to converted EBU-TT-3 document

--- ebu_tt_live/bindings/converters/ebutt1_ebutt3.py
+++ ebu_tt_live/bindings/converters/ebutt1_ebutt3.py
@@ -1,4 +1,8 @@
+import copy
 import logging
+
+from ebu_tt_live.bindings import load_types_for_document, tt1_tt_type, tt
+from pyxb.binding.basis import ElementContent, NonElementContent
 
 log = logging.getLogger(__name__)
 
@@ -9,10 +13,45 @@ class EBUTT1EBUTT3Converter(object):
         pass
 
     def map_type(self, in_element):
-        return self.convert_unknown
+        if isinstance(in_element, tt1_tt_type):
+            return self.convert_tt
+        else:
+            return self.convert_unknown
+
+    def convert_tt(self, tt_in, dataset):
+        document_identifier = 'sequence'
+        if hasattr(tt_in, 'head') and hasattr(tt_in.head, 'metadata'):
+            if hasattr(tt_in.head.metadata, 'documentMetadata') and \
+                hasattr(tt_in.head.metadata.documentMetadata, 'documentIdentifier') and \
+                    tt_in.head.metadata.documentMetadata.documentIdentifier:
+                document_identifier = tt_in.head.metadata.documentMetadata.documentIdentifier
+            elif hasattr(tt_in.head.metadata, 'documentIdentifier') and \
+                    tt_in.head.metadata.documentIdentifier:
+                document_identifier = tt_in.head.metadata.documentIdentifier
+
+        new_elem = tt(
+            sequenceIdentifier=document_identifier,
+            sequenceNumber=1,
+        )
+        return new_elem
+
 
     def convert_unknown(self, element, dataset):
         return None
+
+    def convert_children(self, element, dataset):
+        output = []
+        children = element.orderedContent()
+        for item in children:
+            if isinstance(item, NonElementContent):
+                output.append(copy.deepcopy(item.value))
+            elif isinstance(item, ElementContent):
+                conv_elem = self.convert_element(item.value, dataset)
+                if conv_elem is not None:
+                    output.append(conv_elem)
+            else:
+                raise Exception('Can this even happen!??!?!?!')
+        return output
 
     def convert_element(self, element, dataset):
         converter = self.map_type(element)
@@ -23,6 +62,9 @@ class EBUTT1EBUTT3Converter(object):
             self._semantic_dataset = {}
         else:
             self._semantic_dataset = dataset
+
+        load_types_for_document('ebutt3')
+
         converted_bindings = self.convert_element(
             root_element,
             self._semantic_dataset
--- testing/bdd/test_ebutt1_parsing.py
+++ testing/bdd/test_ebutt1_parsing.py
@@ -1,9 +1,11 @@
 import pytest
 from pytest_bdd import parsers, scenarios, when, then
-from ebu_tt_live.documents import EBUTT1Document
+from ebu_tt_live.documents import EBUTT1Document, EBUTT3Document
+from ebu_tt_live.documents.converters import EBUTT1EBUTT3Converter
 from pyxb.exceptions_ import IncompleteElementContentError, UnrecognizedAttributeError
 
-scenarios('features/ebutt1/ebutt1_validity.feature')
+# scenarios('features/ebutt1/ebutt1_validity.feature')
+scenarios('features/ebutt1/tt1_tt3_conversion.feature')
 
 
 @when(parsers.parse('the document contains a "{element}" element'))
@@ -14,6 +16,11 @@ def when_document_contains_element(template_dict, element):
 @when(parsers.parse('the document body contains a "{attribute}" attribute'))
 def when_document_body_contains_attribute(template_dict, attribute):
     template_dict[attribute] = True
+
+
+@when(parsers.parse('the document has metadata "{key}" set to "{value}"'))
+def when_document_has_metadata(template_dict, key, value):
+    template_dict[key] = value
 
 
 @when('the document\'s timeBase is set to <timebase>')
@@ -36,6 +43,14 @@ def when_document_parsed_ebutt1(test_context, template_file, template_dict):
     test_context['ebutt1_document'] = ebutt1_document
 
 
+@when('the EBU-TT-1 document is converted to EBU-TT-3')
+def when_convert_tt1_tt3(test_context):
+    converter = EBUTT1EBUTT3Converter()
+    ebutt1 = test_context['ebutt1_document']
+    converted_bindings = converter.convert_document(ebutt1.binding)
+    test_context['ebutt3_binding'] = converted_bindings
+
+
 @then('the document fails to parse as an EBU-TT-1 document because of an IncompleteElementContentError')
 def then_document_fails_ebutt1_element(template_file, template_dict):
     xml_text = template_file.render(template_dict)
@@ -56,3 +71,17 @@ def then_document_fails_ebutt1_attribute(template_file, template_dict):
 def then_ebutt1_valid(test_context):
     document = test_context['ebutt1_document']
     document.validate()
+
+
+@then('the EBU-TT-3 document is valid')
+def then_ebutt3_valid(test_context):
+    converted_bindings = test_context['ebutt3_binding']
+    converted_document = EBUTT3Document.create_from_raw_binding(converted_bindings)
+    test_context['ebutt3_document'] = converted_document
+    converted_document.validate()
+
+
+@then(parsers.parse('the tt element contains the attribute "{attribute}" set to "{value}"'))
+def then_ebutt3_tt_has_attribute(test_context, attribute, value):
+    tt = test_context['ebutt3_binding']
+    assert str(getattr(tt, attribute)) == value
